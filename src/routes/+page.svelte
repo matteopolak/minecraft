41cd70831eb76984c3b45c12e37b83df1d627f7e
matteopolak <matteopolak@hotmail.com>
2023-01-12 14:49:57 -0500

    feat: add sort

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,19 +5,28 @@
 		{
 			name: 'Username',
 			sort: false,
+			width: '10%',
+			fixedHeader: true,
+			resizable: true,
 		},
 		{
 			name: 'Popularity',
 			formatter: (cell: number) => {
 				return cell.toFixed(2);
 			},
-			sort: true,
+			width: '10%',
+			fixedHeader: true,
+			resizable: true,
 		},
 		{
 			name: 'Definition',
 			formatter: (cell: string[]) => {
 				return cell.join('\n');
 			},
+			sort: false,
+			width: '50%',
+			fixedHeader: true,
+			resizable: true,
 		},
 	];
 
@@ -27,36 +36,53 @@
 	}
 </script>
 
-<Grid
-	{columns}
-	sort
-	search={{
-		debounceTimeout: 500,
-		server: {
-			url: (prev, keyword) => `${prev}search=${keyword}&`,
-		},
-	}}
-	pagination={{
-		enabled: true,
-		limit: 15,
-		server: {
-			url: (prev, page, limit) =>
-				`${prev}offset=${page * limit}&limit=${limit}&`,
-		},
-	}}
-	server={{
-		url: 'https://api.matteopolak.com/names?',
-		then: data =>
-			// @ts-ignore
-			data.results.map(profile => [
-				profile.username,
-				profile.popularity,
-				profile.definition,
-			]),
-		total: data => data.total,
-	}}
-/>
+<div class="paginated">
+	<Grid
+		{columns}
+		search={{
+			debounceTimeout: 500,
+			server: {
+				url: (prev, keyword) => `${prev}search=${keyword}&`,
+			},
+		}}
+		pagination={{
+			enabled: true,
+			limit: 15,
+			server: {
+				url: (prev, page, limit) =>
+					`${prev}offset=${page * limit}&limit=${limit}&`,
+			},
+		}}
+		server={{
+			url: 'https://api.matteopolak.com/names?',
+			then: data =>
+				// @ts-ignore
+				data.results.map(profile => [
+					profile.username,
+					profile.popularity,
+					profile.definition,
+				]),
+			total: data => data.total,
+		}}
+		sort={{
+			multiColumn: false,
+			server: {
+				url: (prev, columns) => {
+					return `${prev}&sort=${
+						columns[0]?.direction === 1 ? 'asc' : 'desc'
+					}&`;
+				},
+			},
+		}}
+	/>
+</div>
 
 <style global>
 	@import 'https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css';
+	@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&display=swap');
+
+	.paginated {
+		font-family: 'Open Sans', sans-serif;
+		font-size: 1rem;
+	}
 </style>
